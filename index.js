9e2b0c4a9c6b7e430a5a5c1cf55342d91f47129a
CaitlynHagin <caitlyn32h@gmail.com>
2023-08-12 15:38:10 +0100

    working card wooo

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 function createCard(event) {
     document.getElementById("main-container").style = "display: none"
     document.getElementById('fname').value
-
-    console.log(document.getElementById('fname').value)
-    return{
-        name:document.getElementById('fname').value + document.getElementById('lname').value,
-        email:document.getElementById('email').value,
-        number:document.getElementById('number').value,
-    }
+    const fname = document.getElementById('fname').value;
+    const lname = document.getElementById('lname').value;
+    const email = document.getElementById('email').value;
+    const number = document.getElementById('number').value; 
+    
+    const userName = document.getElementById('display-name'); 
+    const userEmail = document.getElementById('display-email'); 
+    const userNumber = document.getElementById('display-number'); 
+    userName.textContent = fname + lname;
+    userEmail.textContent = email;
+    userNumber.textContent = number;
 }
+
 
