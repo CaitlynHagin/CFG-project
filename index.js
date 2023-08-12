740853d01065cf50b8e5fc2d23a304c814965ef0
CaitlynHagin <caitlyn32h@gmail.com>
2023-08-12 15:15:09 +0100

    caits changes

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 function createCard(event) {
     document.getElementById("main-container").style = "display: none"
+    document.getElementById('fname').value
+
+    console.log(document.getElementById('fname').value)
+    return{
+        name:document.getElementById('fname').value + document.getElementById('lname').value,
+        email:document.getElementById('email').value,
+        number:document.getElementById('number').value,
+    }
 }
+
